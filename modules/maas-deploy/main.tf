a9a6297085b7554341af60a3359c84504210025e
Toby de Mendonça <toby.demendonca@canonical.com>
2026-09-15 10:32:10 +0100

    fix: add triggers, depends on, and decouples create_admin

--- modules/maas-deploy/main.tf
+++ modules/maas-deploy/main.tf
@@ -119,6 +119,15 @@ resource "terraform_data" "juju_wait_for_all" {
     )
   }
 
+  # Ensures juju_wait_for_all triggers when any of the following values change
+  # TODO: these trigger values are rough at the moment
+  triggers_replace = {
+    maas_region = sha256(jsonencode(juju_application.maas_region.config))
+    haproxy     = try(juju_application.haproxy[0].config, null)
+    keepalived  = try(juju_application.keepalived[0].config, null)
+    tls_route   = try(juju_integration.maas_haproxy_https[0].id, null)
+  }
+
   provisioner "local-exec" {
     # We need to set JUJU_DATA to a unique directory to avoid conflicts with other juju commands that might be running in parallel,
     # since juju CLI uses a shared state directory by default ($HOME/.local/share/juju).
@@ -147,8 +156,10 @@ resource "terraform_data" "juju_wait_for_all" {
 # TODO: linked to this issue https://github.com/juju/terraform-provider-juju/issues/388
 resource "terraform_data" "create_admin" {
   input = {
-    model = terraform_data.juju_wait_for_all.output.model
+    model = local.maas_model_uuid
   }
+
+  depends_on = [ terraform_data.juju_wait_for_all ]
 
   provisioner "local-exec" {
     # We need to set JUJU_DATA to a unique directory to avoid conflicts with other juju commands that might be running in parallel,
@@ -182,6 +193,8 @@ resource "terraform_data" "create_admin" {
 data "external" "maas_get_api_key" {
   program = ["bash", "${path.module}/scripts/get-api-key.sh"]
 
+  depends_on = [ terraform_data.juju_wait_for_all ]
+
   query = {
     model                   = terraform_data.create_admin.output.model
     username                = var.admin_username
@@ -194,6 +207,7 @@ data "external" "maas_get_api_key" {
 data "external" "maas_get_api_url" {
   program = ["bash", "${path.module}/scripts/get-api-url.sh"]
 
+  depends_on = [ terraform_data.juju_wait_for_all ]
   query = {
     model                   = terraform_data.create_admin.output.model
     juju_controller_address = var.juju_controller.controller_addresses[0]
